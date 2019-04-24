6c79e6b096c021c567d42c1cfc23f09313eea1c2
Ernesto Avila Domenech <eadomenech@gmail.com>
2019-04-24 09:25:55 -0400

    Cambios menores

--- fnn/fnn.py
+++ fnn/fnn.py
@@ -69,10 +69,10 @@ def test(args, model, device, test_loader):
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='FNN')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument(
-        '--test-batch-size', type=int, default=8, metavar='N',
+        '--test-batch-size', type=int, default=64, metavar='N',
         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=100, metavar='N',
                         help='number of epochs to train (default: 10)')
@@ -85,7 +85,7 @@ def main():
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument(
-        '--log-interval', type=int, default=100, metavar='N',
+        '--log-interval', type=int, default=10, metavar='N',
         help='how many batches to wait before logging training status')
 
     parser.add_argument('--save-model', action='store_true', default=True,
@@ -121,6 +121,7 @@ def main():
         num_workers=2)
 
     model = Net().to(device)
+    print(model)
     optimizer = optim.SGD(
         model.parameters(), lr=args.lr, momentum=args.momentum)
 
