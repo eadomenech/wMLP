a3622dbe0a3d6c0a594e0142c29e22dc25ff73c8
Ernesto Avila Domenech <eadomenech@gmail.com>
2020-02-28 11:02:21 -0500

    Visualizando con tensor board

--- 06_fnn/fnn.py
+++ 06_fnn/fnn.py
@@ -5,15 +5,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
-from visualization.visdom import Visualizations
+
+# from visualization.visdom import Visualizations
+
+#Tensorboard
+from logger import Logger
 
 import numpy as np
 from PIL import Image
 import random
 from io import BytesIO
 
-# Initialize the visualization environment
-vis = Visualizations()
+# # Initialize the visualization environment
+# vis = Visualizations()
 
 #Var
 train_loss_list = []
@@ -25,11 +29,11 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(3 * 8 * 8, 300)
-        self.fc2 = nn.Linear(300, 10)
+        self.fc2 = nn.Linear(300, 9)
     
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.dropout(x)
+        x = F.dropout(x, p=0.5, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
@@ -109,13 +113,13 @@ def main():
     # Training settings
     parser = argparse.ArgumentParser(description='FNN')
     parser.add_argument(
-        '--batch-size', type=int, default=1000, metavar='N',
+        '--batch-size', type=int, default=10, metavar='N',
         help='input batch size for training (default: 64)')
     parser.add_argument(
-        '--test-batch-size', type=int, default=100, metavar='N',
+        '--test-batch-size', type=int, default=10, metavar='N',
         help='input batch size for testing (default: 1000)')
     parser.add_argument(
-        '--epochs', type=int, default=600, metavar='N',
+        '--epochs', type=int, default=100, metavar='N',
         help='number of epochs to train (default: 10)')
     parser.add_argument(
         '--lr', type=float, default=0.01, metavar='LR',
@@ -149,7 +153,8 @@ def main():
         [
             transforms.Lambda(randomJpegCompression),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.Normalize(
+                [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
     )
 
@@ -159,14 +164,19 @@ def main():
 
     # Data loader
     train_loader = torch.utils.data.DataLoader(
-        dataset=train_dataset, batch_size=args.batch_size, shuffle=True,
-        num_workers=2)
+        dataset=train_dataset, batch_size=args.batch_size,
+        shuffle=True, num_workers=2)
 
     test_loader = torch.utils.data.DataLoader(
-        dataset=valid_dataset, batch_size=args.test_batch_size, shuffle=False, num_workers=2)
+        dataset=valid_dataset, batch_size=args.test_batch_size,
+        shuffle=False, num_workers=2)
 
     model = Net().to(device)
     print(model)
+    
+    # Tensorboard
+    logger = Logger('./logs')
+
     optimizer = optim.SGD(
         model.parameters(), lr=args.lr, momentum=args.momentum)
 
@@ -175,13 +185,29 @@ def main():
             args, model, device, train_loader, optimizer, epoch)
         dic = test(args, model, device, test_loader, epoch)
 
-        # Visualization data
-        vis.plot_loss_train(global_loss_train, epoch)
-        vis.plot_loss_valid(dic['test_loss'], epoch)
-        vis.plot_acc(dic['acc'], epoch)
+        # # Visualization data
+        # vis.plot_loss_train(global_loss_train, epoch)
+        # vis.plot_loss_valid(dic['test_loss'], epoch)
+        # vis.plot_acc(dic['acc'], epoch)
+
+        # Tensorboard
+        # 1. Log scalar values (scalar summary)
+        info = { 
+            'loss_train': global_loss_train,
+            'loss_valid': dic['test_loss'],
+            'accuracy': dic['acc'] }
+
+        for tag, value in info.items():
+            logger.scalar_summary(tag, value, epoch)
+
+        # 2. Log values and gradients of the parameters (histogram summary)
+        for tag, value in model.named_parameters():
+            tag = tag.replace('.', '/')
+            logger.histo_summary(tag, value.data.cpu().numpy(), epoch)
+            logger.histo_summary(tag+'/grad', value.grad.data.cpu().numpy(), epoch)
 
     if (args.save_model):
-        torch.save(model.state_dict(), "fnnNew_600epoch_1xlayers385.pt")
+        torch.save(model.state_dict(), "fnnNew_10epoch_1xlayers300.pt")
 
 if __name__ == '__main__':
     main()
